633e8cb542801a4644b32f49565e5a63ba344f20
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2025-10-15 12:32:07 +0100

    ..

--- apps/www/src/app/page.tsx
+++ apps/www/src/app/page.tsx
@@ -2,10 +2,7 @@ import { db } from "@gnd/db";
 import Image from "next/image";
 
 export default async function Home() {
-  await db.users.create({
-    data: {},
-  });
-  const x = await db.users.findMany({
+  const x = await db.users.count({
     where: {
       deletedAt: null,
     },
@@ -13,7 +10,7 @@ export default async function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        {JSON.stringify(x)}
+        {JSON.stringify({ x })}
         <Image
           className="dark:invert"
           src="/next.svg"
